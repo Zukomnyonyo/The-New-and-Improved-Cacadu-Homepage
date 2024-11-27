b053ecfc4b686ad290070c0895f06c65546bf121
Zukomnyonyo <zukomnyonyo5@gmail.com>
2024-11-27 22:37:30 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// javascript
+
+var sign_up;
+
+
+sign_up = [];
+
+
+document.getElementById('sign-up-button').addEventListener('click', (event) => {
+  let element_Enter_Email = document.getElementById('Enter-Email');
+  element_Enter_Email.innerText = '';
+
+});
+sign_up;
